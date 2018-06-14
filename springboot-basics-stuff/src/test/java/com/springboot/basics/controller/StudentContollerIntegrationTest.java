240c8329044b5cea1cd7963834f82c683be756b7
Karan Biawat <biawat.karan@gmail.com>
2018-06-14 22:00:32 +0100

    spring boot - tests added

--- springboot-basics-stuff/src/test/java/com/springboot/basics/controller/StudentContollerIntegrationTest.java
+++ springboot-basics-stuff/src/test/java/com/springboot/basics/controller/StudentContollerIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.springboot.basics.controller;
 
 import com.springboot.basics.SpringbootBasicsStuffApplication;
+import com.springboot.basics.model.Course;
+import com.sun.javafx.scene.control.skin.VirtualFlow;
 import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +16,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringbootBasicsStuffApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StudentContollerIntegrationTest {
@@ -25,7 +32,7 @@ public class StudentContollerIntegrationTest {
     HttpHeaders headers = new HttpHeaders();
 
     @Test
-    public void testRetrieveStudentCourse() throws JSONException {
+    public void getServiceIntegrationTest() throws JSONException {
 
         HttpEntity<String> entity = new HttpEntity<>(null,headers);
 
@@ -33,6 +40,20 @@ public class StudentContollerIntegrationTest {
 
         String expected = "{\"id\":\"Course1\",\"name\":\"Spring\",\"description\":\"10 Steps\"}";
         JSONAssert.assertEquals(expected, response.getBody(), false);
+    }
+
+    @Test
+    public void postServiceIntegrationTest() throws JSONException {
+
+        Course course = new Course("postIntegrationTestID","postIntegrationTest","Post Integration Test Description", Arrays.asList("Spring Boot Microservices"));
+
+        HttpEntity<Course> entity = new HttpEntity<>(course, headers);
+
+        ResponseEntity<Course> response = restTemplate.exchange(createURLWithPort("/students/Student1/courses"), HttpMethod.POST, entity, Course.class);
+
+        String location = response.getHeaders().get(HttpHeaders.LOCATION).get(0);
+        assertTrue(location.contains("/students/Student1/courses"));
+
     }
 
     private String createURLWithPort(String uri){
